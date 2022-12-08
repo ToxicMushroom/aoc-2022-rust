5302c6b65d92d259b396c32aa8f100fae2dd3ff2
ToxicMushroom <32853531+ToxicMushroom@users.noreply.github.com>
2022-12-08 02:02:45 +0100

    feat: epic prolog solution, unbeatable

--- prolog/day1.pl
+++ prolog/day1.pl
@@ -1,50 +1,34 @@
 % prolog
 :- use_module(library(clpfd)).
 :- use_module(library(dcg/basics)).
-% :- table numbers//0.
 
-elve1([N|Ns]) --> integer2(N), eol, elve(Ns).
-elve([N|Ns]) --> integer2(N), eol, elve(Ns).
+elve1([N|Ns]) --> integer(N), eol, elve(Ns).
+elve([N|Ns]) --> integer(N), eol, elve(Ns).
 elve([]) --> [].
 
-integer2(N) --> numbers(L), { number_codes(N, L) }.
-numbers([A|As]) --> [A], { char_type(A, digit) }, all_digits(As).
-all_digits([A|As]) --> [A], { char_type(A, digit) }, all_digits(As).
-all_digits([]) --> [].
+% Obsolete due to dcg/basics' integer//1 predicate.
+% integer2(N) --> numbers(L), { number_codes(N, L) }.
+% numbers([A|As]) --> [A], { char_type(A, digit) }, all_digits(As).
+% all_digits([A|As]) --> [A], { char_type(A, digit) }, all_digits(As).
+% all_digits([]) --> [].
 
 elves([E|Es]) --> elve1(E), eol, elves(Es).
 elves([]) --> [].
 
-sum([], 0).
-sum([H|T], A) :- 
-    A #= H + A2, 
-    sum(T, A2).
-
-find_max([B], B).    
-find_max([H|T], A) :-
-    A #= max(H, A2),
-    find_max(T, A2).
-
-find_max_sum([B], R) :- sum(B, R).
-find_max_sum([H|T], MS) :-
-    MS #= max(RS, RMS),
-    sum(H, RS),
-    find_max_sum(T, RMS).
-
 insert(E, [], [E]).
 insert(E, [H|L], N) :- 
-    (E #=< H, N=E|[H|L]);
-    (E #> H, N = [H | Rest], insert(E, L, N)).
+    (E #=< H, N = [E| [H|L]]); (E #> H, N = [H | Rest], insert(E, L, Rest)).
 
-% find_max_3sum([B], R) :- .
-% find_max_3sum([H|T], MS) :- 
-find_3max([H|T], R) :- 
-    (length(R, 3), insert(sum(H), R, N), N = [HA|R2], find_3max(T, R2));
-    (length(R, LR), LR #< 3, insert(sum(H), R, R2), find_3max(T, R2)).
-    
-    
+find_3max([], R, R).
+find_3max([H|T], R, RR) :- 
+    sum_list(H, S),
+    ((length(R, 3), insert(S, R, N), N = [_|R2], find_3max(T, R2, RR));
+    (length(R, LR), LR #< 3, insert(S, R, R2), find_3max(T, R2, RR))).
 
-solve(R, R2) :-
+solve(R1, R2) :-
     phrase_from_file(elves(N), "../input/day1"),
-    find_max_sum(N, R),
-    find_max_3sum(N, R).
+    find_3max(N, [], M3),
+    [_, _, R1] = M3,
+    sum_list(M3, R2),
+    writeln(R1),
+    writeln(R2).
